a833b582239756b2cac342694f7a3af886fd78a0
K-MHOON <azmria117@gmail.com>
2025-05-25 18:26:28 +0900

    feat : 이미지 기본 설정 및 DTO 추가

---
+++ openaiImage/src/main/java/com/kmhoon/openaiImage/OpenaiImageApplication.java
@@ -0,0 +1,12 @@
+package com.kmhoon.openaiImage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenaiImageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenaiImageApplication.class, args);
+    }
+}
